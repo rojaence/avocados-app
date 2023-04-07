1cf13e2f68dba5e3a933c0b0625e21e48ae5b3a6
Ronny Endara <rojaence@gmail.com>
2023-04-06 21:55:31 -0500

    UPDATE initial data load in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar/Navbar";
+import { IProduct } from "../services/product/product.model";
 
 function Index() {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  useEffect(() => {
+    fetch("api/avo")
+      .then((res) => res.json())
+      .then(({ data, length }) => setProducts(data));
+  }, []);
+
   return (
     <div>
       <Navbar />
       <h1>Hola mundo</h1>
+      <p>avocados</p>
+      <ul>
+        {products.map((product) => (
+          <li key={product.id}>{product.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
