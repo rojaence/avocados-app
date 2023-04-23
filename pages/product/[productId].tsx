8e2410b4407038baa11c71e9e1ebb517b5b48995
Ronny Endara <rojaence@gmail.com>
2023-04-22 21:33:23 -0500

    UPDATE serverSideProps in productId page

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Head from "next/head";
-import { GetStaticPaths, GetStaticPropsContext } from "next";
+import {
+  GetServerSidePropsContext,
+  GetStaticPaths,
+  GetStaticPropsContext,
+} from "next";
 import { IProduct } from "../../services/product/product.model";
 import ProductDetail from "../../containers/ProductDetail";
 import { serverUrl } from "../../config";
@@ -10,24 +14,9 @@ interface Props {
   data: IProduct;
 }
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const res = await fetch(`${serverUrl}/api/avo`);
-  const { data: products }: IAPIAvoResponse = await res.json();
-
-  const paths = products.map(({ id }) => ({
-    params: {
-      productId: id,
-    },
-  }));
-
-  return {
-    paths,
-    // Fallback: false => 404 for everything else
-    fallback: false,
-  };
-};
-
-export const getStaticProps = async ({ params }: GetStaticPropsContext) => {
+export const getServerSideProps = async ({
+  params,
+}: GetServerSidePropsContext) => {
   const res = await fetch(`${serverUrl}/api/avo/${params?.productId}`);
   const data: IProduct = await res.json();
   return {
